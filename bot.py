715da4ca1ed1436ed30125575df4aa3122371541
DarkvibeX <bahtiyorovbahtiyor041@gmail.com>
2026-04-20 18:27:27 +0500

    Add files via upload

--- bot.py
+++ bot.py
@@ -11,7 +11,7 @@ dp = Dispatcher()
 CARD_NUMBER = "5614 6822 1054 1860"
 LOCATION_LINK = "https://maps.google.com/?q=Megaplanet+Tashkent"
 
-# 🛒 ТОВАРЫ
+
 products = {
      "Booppin bunny": 285000,
     "Spooky and Pumpkin": 350000,
@@ -25,33 +25,33 @@ products = {
     "Gym Broos": 1000000,
     "Camera Ramena": 60000,
     "Rosey and Teddy": 600000,
-    "Los chilinis": 1300000,
+    #"Los chilinis": 1300000,
     "Fortunu and Cashuru": 250000,
     "Dragon Caneloni": 1000000,
     "Hydra Dragon": 1450000,
-    "Dragon Gingerini": 2000000,
-    "Rico Dinero": 1700000,
-    "Acrodragon": 6000000,
-    "Pancake and Syrup": 4000000,
+    #"Dragon Gingerini": 2000000,
+    #"Rico Dinero": 1700000,
+    #"Acrodragon": 6000000,
+    #"Pancake and Syrup": 4000000,
     "Cookie and Milki": 270000,
     "Los Amigos": 270000,
     "Los Sekolah": 300000,
     "Ketupat Bros": 870000,
-    "Elefanto Frigo": 1800000,
+    #"Elefanto Frigo": 1800000,
     "Cloverat Calapat": 200000,
-    "Signore Carapace": 4000000,
+    #"Signore Carapace": 4000000,
     "Skibidi Toilet": 40000000,
     "Griffin": 50000000,
-    "Headless Horsemen": 150000000,
+    #"Headless Horsemen": 150000000,
     "Hydra Bunny": 1800000,
     "Bunny and Eggy": 1400000,
     "Meowl": 70000000,
-    "Strawberry Elephant": 100000000
+    #"Strawberry Elephant": 100000000
 }
 
 user_cart = {}
 
-# 📦 клавиатура товаров
+
 def product_kb():
     kb = []
     row = []
@@ -69,7 +69,7 @@ def product_kb():
 
     return ReplyKeyboardMarkup(keyboard=kb, resize_keyboard=True)
 
-# 💳 оплата
+
 pay_kb = ReplyKeyboardMarkup(
     keyboard=[
         [KeyboardButton(text="💵 Наличные")],
@@ -78,7 +78,7 @@ pay_kb = ReplyKeyboardMarkup(
     resize_keyboard=True
 )
 
-# /start
+
 @dp.message(F.text == "/start")
 async def start(message: Message):
     await message.answer(
@@ -86,7 +86,7 @@ async def start(message: Message):
         reply_markup=product_kb()
     )
 
-# выбор товара
+
 @dp.message(lambda m: m.text in products)
 async def select_product(message: Message):
 
@@ -99,7 +99,7 @@ async def select_product(message: Message):
         reply_markup=pay_kb
     )
 
-# 💵 наличка
+
 @dp.message(F.text == "💵 Наличные")
 async def cash(message: Message):
 
@@ -124,7 +124,7 @@ async def cash(message: Message):
 
     await message.answer("✅ Заказ отправлен владельцу")
 
-# 💳 карта
+
 @dp.message(F.text == "💳 Карта")
 async def card(message: Message):
 
@@ -148,7 +148,7 @@ async def card(message: Message):
         f"⚠️ Ожидается чек"
     )
 
-# 📸 чек
+
 @dp.message(F.photo)
 async def check(message: Message):
 
@@ -164,7 +164,7 @@ async def check(message: Message):
 
     await message.answer("✅ Чек получен! Скоро свяжемся.")
 
-# запуск
+
 async def main():
     print("DarkStoreX PRO запущен...")
     await dp.start_polling(bot)
